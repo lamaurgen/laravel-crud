a99e76d2560693cedbb3aec93e59d124675e3849
lamaurgen <65845025+lamaurgen@users.noreply.github.com>
2022-12-16 17:14:47 +0545

    cr updated

---
+++ nis/resources/views/students/create.blade.php
@@ -0,0 +1,42 @@
+@extends('students.layout')
+@section ('content')
+
+
+<form action="{{ route('students.store') }}" method="POST">
+    @csrf
+    <div class="mb-3">
+      <label>Name </label>
+      <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  value="{{ old('name') }}">
+      
+    </div>
+    <div class="mb-3">
+        <label>Address </label>
+        <input type="text" name="address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ old('address') }}">
+        
+      </div>
+      <div class="mb-3">
+        <label>Email </label>
+        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ old('email') }}">
+        
+      </div>
+      <div class="mb-3">
+        <label>Number</label>
+        <input type="number" name="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ old('number') }}">
+        
+      </div>
+      {{-- Gender --}}
+      <div>
+      <h3>Select Gender</h3>
+      <input type="radio" name="gender" value="male" value="{{ old('male') }}">Male
+      <br>
+    <input type="radio" name="gender" value="female" value="{{ old('female') }}">Female
+    <br>
+      <input type="radio" name="gender" value="others" value="{{ old('others') }}">Others
+
+  </div>
+      <br>
+
+
+    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+  </form>
+@endsection
---
+++ nis/resources/views/students/index.blade.php
@@ -0,0 +1,49 @@
+@extends('students.layout')
+@section('content')
+<h1>
+    Students List
+</h1>
+<div class="pull-right">
+    <a class="btn btn-success" href="{{ route('students.create') }}"> Add New</a>
+</div>
+<br>
+<table class="table table-bordered">
+    <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Email</th>
+        <th>Number</th>
+        <th>Gender</th>
+
+        <th width="280px">Action</th>
+    </tr>
+    @foreach ($students as $student)
+    <tr>
+        <td>{{ ++$i }}</td>
+        <td>{{ $student->name }}</td>
+        <td>{{ $student->address}}</td>
+        <td>{{ $student->email}}</td>
+        <td>{{ $student->number}}</td>
+        <td>{{ $student->gender}}</td>
+
+        <td>
+            <form action="{{ route('students.destroy',$student->id) }}" method="POST">
+
+                <a class="btn btn-info" href="{{ route('students.show',$student->id) }}">Show</a>
+
+                <a class="btn btn-primary" href="{{ route('students.edit',$student->id) }}">Edit</a>
+
+                @csrf
+                @method('DELETE')
+  
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+{!! $students->links() !!}
+</table>
+@endsection
